8ffb2e2595702ea21290a365862ba87c7d46873c
Remy Orans <remy.orans@gmail.com>
2021-01-04 16:39:07 -0800

    Implement clean up with table and one event listener

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,3 +1,22 @@
+let render = {
+  renderState: function(row, col, toRender) {
+    let gridElement = document.getElementsByTagName('table')[0].tBodies[0].children[row].children[col];
+    gridElement.textContent = toRender;
+  },
+
+  clearState: function() {
+    let rows = [0, 1, 2];
+    let cols = [0, 1, 2];
+    for (let row of rows) {
+      for (let col of cols) {
+        console.log('clearing');
+        render.renderState(row, col, '');
+      }
+    }
+  }
+
+}
+
 // State:
 class Storage {
   constructor() {
@@ -6,31 +25,43 @@ class Storage {
   }
 
   addToState(row, col) {
-    let rows = {row: 0, row1: 1, row2: 2};
-    let cols = {one: 0, two: 1, three: 2};
-    if (this.numTurns % 2 === 0) {
-      this._state[rows[row]][cols[col]] = 'X';
-    } else {
-      this._state[rows[row]][cols[col]] = 'O';
+    if (!this._state[row][col]) {
+      console.log(row, col);
+      if (this.numTurns % 2 === 0) {
+        this._state[row][col] = 'X';
+      } else {
+        this._state[row][col] = 'O';
+      }
+
+      this.numTurns++;
+      render.renderState(row, col, this._state[row][col]);
+
+      this.checkWin();
     }
-    this.numTurns++;
-    renderState(this);
-    this.checkWin();
+
   }
 
   checkWin() {
     let potentialWin = this.checkDiagnols();
     if (potentialWin) {
-      alert('Player ' + potentialWin + ' has won!');
+      this.onWin(potentialWin)
     }
     potentialWin = this.checkHorizontals();
     if (potentialWin) {
-      alert('Player ' + potentialWin + ' has won!');
+      this.onWin(potentialWin);
     }
     potentialWin = this.checkVerticals();
     if (potentialWin) {
-      alert('Player ' + potentialWin + ' has won!');
+      this.onWin(potentialWin);
     }
+    if (this.numTurns === 9) {
+      alert('Draw');
+    }
+  }
+
+  onWin(whichPlayer) {
+    alert('Player ' + whichPlayer + ' has won!');
+    render.clearState();
   }
 
   checkDiagnols() {
@@ -80,59 +111,18 @@ class Storage {
 
 }
 
-// Set up event listeners
-let rows = ['row', 'row1', 'row2'];
-let columns = [0, 1, 2];
+
 let storage = new Storage();
 
-for (let row of rows) {
-  for (let column of columns) {
-    let element = document.getElementsByClassName(row)[0].children[column];
-    element.addEventListener('click', event => {
-      console.log(event.path[0].id, '\n', event.path[1].className);
-      storage.addToState(event.path[1].className, event.path[0].id);
-    })
-  }
-}
+let element = document.getElementsByTagName('table')[0];
+element.addEventListener('click', event => {
+  console.log(event);
+  storage.addToState(event.path[1].rowIndex, event.path[0].cellIndex);
+});
 
 document.getElementById('restart').addEventListener('click', event => {
-  console.log('clicked');
   storage = new Storage();
-  clearState();
+  render.clearState();
 })
 
-// RenderState
-function renderState(storage) {
-  let rows = [0, 1, 2];
-  let cols = [0, 1, 2];
-  let rowDict = {0: 'row', 1: 'row1', 2: 'row2'};
-
-  for (let row of rows) {
-    for (let col of cols) {
-      if (storage._state[row][col]) {
-        let element = document.createElement('span');
-        element.textContent = storage._state[row][col];
-        element.className = 'piece';
-        let gridElement = document.getElementsByClassName(rowDict[row])[0].children[col];
-        if (gridElement.children.length === 0) {
-          document.getElementsByClassName(rowDict[row])[0].children[col].append(element);
-        }
-
-      }
-    }
-  }
-}
-
-function clearState() {
-  let rows = [0, 1, 2];
-  let cols = [0, 1, 2];
-  let rowDict = {0: 'row', 1: 'row1', 2: 'row2'};
-
-  for (let row of rows) {
-    for (let col of cols) {
-      let gridElement = document.getElementsByClassName(rowDict[row])[0].children[col];
-      gridElement.innerHTML = '';
-    }
-  }
-}
 
